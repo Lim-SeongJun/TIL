96beed75f89563ace8209d847b5f6c47e5e8dc47
Lim-SeongJun <dlatjdwns331@naver.com>
2024-06-17 16:21:29 +0900

    decorator ADD

--- de/airflow/dags/dags_python_import_func.py
+++ de/airflow/dags/dags_python_import_func.py
@@ -7,7 +7,7 @@ from common.common_func import get_sftp
 with DAG(
     dag_id="dags_python_import_func",
     schedule="30 6 * * *",
-    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2024, 6, 17, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
 
